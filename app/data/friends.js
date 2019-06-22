aba655875c363cdaa888bcc8a50cd74fa616cad7
tktommykline <tktommykline@gmail.com>
2019-06-22 03:07:01 -0400

    first commit

---
+++ app/data/friends.js
@@ -0,0 +1,24 @@
+var friends = { 
+    name: Bill,
+    scores: [
+        2,
+        6,
+        4
+    ],
+
+    name: Jim,
+    scores: [
+        9,
+        1,
+        7
+    ],
+
+    name: Bob,
+    scores: [
+        8,
+        9,
+        3
+    ]
+};
+
+module.exports = friends
